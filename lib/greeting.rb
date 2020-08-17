2019837f687ef7034bb62fc5a927a42eb025d255
Demetrio Lima <dlima.nyit@gmail.com>
2020-08-17 22:31:52 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,5 +1,5 @@
 # code the #greeting method here
 def greeting(name)
   puts "Hi! I'm HAL, what's your name?"
-  puts "Hello #{name}, nice to meet you"
+  puts "Hello #{name}, nice to meet you."
 end
